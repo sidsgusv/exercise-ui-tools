91bd7a9a45288db86c0bd6e8162c4047a5911613
Eliza Ivanova <95411237+sidsgusv@users.noreply.github.com>
2026-07-03 04:47:43 +0300

    Adding logic in basic calc

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+def add(first_number: int, second_number: int):
+    """Returns the sum of two numbers."""
+    return first_number + second_number
+
+def subtract(first_number: int, second_number: int):  
+    """Returns the difference of two numbers."""
+    return first_number - second_number 
+
+
+
