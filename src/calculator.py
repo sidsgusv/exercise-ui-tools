5adbab6045736d02125b8e34dd878731e92cbc6f
Eliza Ivanova <95411237+sidsgusv@users.noreply.github.com>
2026-07-03 18:35:24 +0300

    Adding multiplaing functionality

--- src/calculator.py
+++ src/calculator.py
@@ -6,5 +6,9 @@ def subtract(first_number: int, second_number: int):
     """Returns the difference of two numbers."""
     return first_number - second_number 
 
+def multiply(first_number: int, second_number: int):
+    """Returns the product of two numbers."""
+    return first_number * second_number
+
 
 
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,3 +9,14 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(3, 5) == -2
     assert subtract(0, 0) == 0  
+
+    
+
+
+
+
+    def test_multiply():
+        from src.calculator import multiply
+        assert multiply(2, 3) == 6
+        assert multiply(-1, 1) == -1
+        assert multiply(0, 5) == 0
